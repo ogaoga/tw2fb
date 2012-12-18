3c66ce10f16e8ebddc9a369bbf4879d0638d3c95
ogaoga <info@ogaoga.org>
2012-12-18 23:16:52 +0900

    map.

--- script.js
+++ script.js
@@ -19,12 +19,16 @@ if (Meteor.isClient) {
         var reader = new FileReader();
         reader.onload = function(event) {
           var data = {'statuses': JSON.parse(this.result)};
-
           for ( var i = 0 ; i < data.statuses.length ; i++ ) {
+            // parse tweet
             var temp = twttr.txt.autoLink(data.statuses[i].text);
             data.statuses[i].text = temp;
+            // geo 
+            if ( data.statuses[i].geo ) {
+              data.statuses[i].geo.lat = data.statuses[i].geo.coordinates[0];
+              data.statuses[i].geo.lng = data.statuses[i].geo.coordinates[1];
+            }
           }
-
           var str = Template.timeline(data);
           $('#timeline').html(str);
         }
